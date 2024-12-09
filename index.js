21829e08c9a48edd723aa2b3947f5d25d5d5eb6a
Hasnain Raza <dev.hasnain01@gmail.com>
2024-12-09 22:17:45 +0500

    JavaScript Exercise 05

---
+++ index.js
@@ -0,0 +1,218 @@
+// Question no. 01
+// Solution
+
+// let a = 3;
+// let b = 5;
+// let sum = a + b;
+// document.write("Sum of 3 and 5 is " + sum);
+
+// Question no. 02
+// Solution
+// For subtraction
+
+// let a = 3;
+// let b = 5;
+// let subtraction = a - b;
+// document.write("Subtration of 5 from 3 is " + subtraction);
+
+// For Multiplication
+
+// let a = 3;
+// let b = 5;
+// let multiplication = a * b;
+// document.write("Multplication of 3 and 5 is " + multiplication);
+
+// For Division
+
+// let a = 3;
+// let b = 5;
+// let division = 3/5;
+// document.write("Division of 3 and 5 is " + division);
+
+// For Modulus
+
+// let a = 3;
+// let b = 5;
+// let modulus = b % a;
+// document.write("Modulus of 3 and 5 is " + modulus);
+
+// Question no. 03
+// Solution
+
+// Part a
+
+// let newVariable;
+
+// Part b
+
+// document.write("Value after variable declaration is " + newVariable + "<br/>");
+
+// Part c
+
+// newVariable = 5;
+
+// Part d
+
+// document.write("Initial Value : " + newVariable + "<br/>");
+
+// Part e
+
+// newVariable = ++newVariable;
+
+// Part f
+
+// document.write("Value after increment : " + newVariable + "<br/>");
+
+// Part g
+
+// newVariable = newVariable + 7;
+
+// Part h
+
+// document.write("Value after addition : " + newVariable + "<br/>");
+
+// Part i
+
+// newVariable = --newVariable;
+
+// Part j
+
+// document.write("Value after decrement : " + newVariable + "<br/>");
+
+// Part k
+
+// let modulus = newVariable % 3;
+
+// Part l
+
+// document.write("The remainder is : " + modulus);
+
+// Question no. 04
+// Solution
+
+// let oneTicketPrice = 600;
+// let numberOfTickets = 5;
+// document.write(`Total cost to buy ${numberOfTickets} tickets to a movie is ${oneTicketPrice * numberOfTickets}`);
+
+// Question no. 05
+// Solution
+
+// let table = 6;
+// document.write(`Table of ${table} <br/>`);
+// document.write(`${table} x 1 = ${table * 1} <br/>`);
+// document.write(`${table} x 2 = ${table * 2} <br/>`);
+// document.write(`${table} x 3 = ${table * 3} <br/>`);
+// document.write(`${table} x 4 = ${table * 4} <br/>`);
+// document.write(`${table} x 5 = ${table * 5} <br/>`);
+// document.write(`${table} x 6 = ${table * 6} <br/>`);
+// document.write(`${table} x 7 = ${table * 7} <br/>`);
+// document.write(`${table} x 8 = ${table * 8} <br/>`);
+// document.write(`${table} x 9 = ${table * 9} <br/>`);
+// document.write(`${table} x 10 = ${table * 10} <br/>`);
+
+// Question no. 06
+// Solution
+
+// Celcius to Fahrenheit
+
+// let celcius = 25;
+// let celciusToFahrenheit = (celcius * (9 / 5) + 32);
+// document.write(`${celcius}<sup>o</sup>C is ${celciusToFahrenheit}<sup>o</sup>F <br/>`);
+
+// Fahrenheit to Celcius
+
+// let fahrenheit = 70;
+// let fahrenheitToCelcius = ((fahrenheit - 32) * (5/9));
+// document.write(`${fahrenheit}<sup>o</sup>F is ${fahrenheitToCelcius}<sup>o</sup>C`);
+
+// Question no. 07
+// Solution
+
+// let itemOnePrice = 650;
+// let quantityOfItemOne = 3;
+// let totalCostOfItemOne = 650 * 3;
+// let itemTwoPrice = 100;
+// let quantityOfItemTwo = 7;
+// let totalCostOfItemTwo = 100 * 7;
+// let shippingCharges = 100;
+
+// document.write(`Price of Item 1 is : ${itemOnePrice} <br/>`);
+// document.write(`Quantity of item 1 is : ${quantityOfItemOne} <br/>`);
+// document.write(`Price of Item 2 is ${itemTwoPrice} <br/>`);
+// document.write(`Quantity of Item 2 is : ${quantityOfItemTwo} <br/>`);
+// document.write(`Shipping Charges : ${shippingCharges} <br/><br/>`);
+// document.write(`Total cost of your order with delivery charges is ${totalCostOfItemOne + totalCostOfItemTwo + shippingCharges}`);
+
+// Question no. 08
+// Solution
+
+// document.write("<h1>Mark Sheet</h1>");
+
+// let totalMarks = 980;
+// let obtainedMarks = 804;
+// let percentage = ((obtainedMarks / totalMarks) * 100);
+
+// document.write(`Total Marks : ${totalMarks}<br/>`);
+// document.write(`Marks Obtained : ${obtainedMarks}<br/>`);
+// document.write(`Percentage : ${percentage}`);
+
+// Question no. 09
+// Solution
+
+// document.write("<h1>Currency in PKR</h1>");
+
+// let dollarRate = 104.80;
+// let quantityOfUSDollar = 10;
+// let rialRate = 28;
+// let quantityOfSaudiRials = 25;
+// let currencyInPKR = ((quantityOfUSDollar * dollarRate) + (quantityOfSaudiRials * rialRate));
+// document.write(`Total Currency in PKR : ${currencyInPKR}`);
+
+// Question no. 10
+// Solution
+
+// let number = 6;
+// let calculation = (((6 + 5) * 10) / 2);
+// document.write(calculation);
+
+// Question no. 11
+// Solution
+
+// document.write("<h1>Age Calculator</h1>");
+// let currentYear = 2024;
+// let birthYear = 1999;
+// let age = currentYear - birthYear;
+// document.write(`Your age is ${age}`);
+
+// Question no. 12
+// Solution
+
+// document.write("<h1>The Geometizer</h1>");
+
+// let radiusOfCircle = 20;
+// let pi = 3.142;
+// let circumference = (2 * pi * radiusOfCircle);
+// let areaOfCircle = (pi * (radiusOfCircle * radiusOfCircle));
+
+// document.write(`Raduis of circle : ${radiusOfCircle}<br/>`);
+// document.write(`The circumference is :  ${circumference}<br/>`);
+// document.write(`The area of circle is ${areaOfCircle}`);
+
+// Question no. 13
+// Solution
+
+// document.write("<h1>The Lifetime Supply Calculator</h1>");
+
+// let favSnack = "Ban Kabab";
+// let currentAge = 25;
+// let maximumAge = 100;
+// let estimatedAmountPerDay = 2;
+// let finalCalculation = ( (maximumAge - currentAge) * estimatedAmountPerDay);
+
+// document.write(`Favourite Snack : ${favSnack}<br/>`);
+// document.write(`Current Age ${currentAge}<br/>`);
+// document.write(`Estimated Maximum Age ${maximumAge}<br/>`);
+// document.write(`Amount of Snacks Per Day ${estimatedAmountPerDay}<br/>`);
+// document.write(`You will need ${finalCalculation} ${favSnack} to last until the ripe old age of ${maximumAge}`);
+
+// Assignment Complete
